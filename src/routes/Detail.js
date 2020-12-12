7cfc3149e799239131e25f32738d217a4775c002
seongjunhwang <samkukzi3@gmail.com>
2020-12-12 22:37:56 +0900

    apollo setting

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,2 +1,3 @@
 import React from "react";
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => <h1>Detail</h1>;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,24 @@
 import React from "react";
-export default () => <h1>Home</h1>;
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+    const { loading, data } = useQuery(GET_MOVIES);
+    if (loading) {
+      return "loading...";
+    }
+    if (data && data.movies) {
+      return data.movies.map(m => <h1>{m.id}</h1>);
+    }
+  };
